3ad29c45d39869218bb966806888b4b797024a3a
Gustavo Fernandes <gustavofernandes330@gmail.com>
2023-03-06 16:36:23 -0300

    Criação da função main e criação da tabela de caracteres

---
+++ ROT13.py
@@ -0,0 +1,7 @@
+tabelaInterna = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
